8109fe28fe72f34314232b1058bde55c15490f84
Lavrov Sergey <lavrov-sergey@yandex.ru>
2021-02-27 12:25:08 +0300

    Create project

---
+++ app/src/test/java/club/plus1/state_of_the_cloud/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package club.plus1.state_of_the_cloud;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
